d6be9a009155665ca70d3d83815134dc8f0e2427
tracer-ics2024 <tracer@njuics.org>
2024-09-20 19:13:58 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:13:58 up  8:58,  3 users,  load average: 0.02, 0.03, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -29,7 +29,7 @@ void isa_reg_display() {
   printf("Registers status:\n");
   int i;
   for (i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); ++i) {
-    printf("%s\t0x%x\t%d\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
+    printf("%s\t\t0x%x\t\t%d\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
   }
 }
 
