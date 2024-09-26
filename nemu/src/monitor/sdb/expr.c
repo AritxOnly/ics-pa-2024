55eca3b8935e226cb20a4b720246046fdcb7d9f1
tracer-ics2024 <tracer@njuics.org>
2024-09-26 09:38:19 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:38:19 up  1:26,  3 users,  load average: 0.25, 0.11, 0.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -52,7 +52,7 @@ static struct rule {
   {" +", TK_NOTYPE},    // space
   {"^0x[0-9A-F]+", TK_INT_HEX},  // 16 base integers
   {"[0-9]+", TK_INT_DEC}, // 10 base integers
-  {"$[$a-z0-9]+", TK_REG_NAME}, // register names
+  {"$[a-z0-9]+", TK_REG_NAME}, // register names
 
   {"\\(", TK_BRACKET_L},  // left bracket
   {"\\)", TK_BRACKET_R},  // right bracket
