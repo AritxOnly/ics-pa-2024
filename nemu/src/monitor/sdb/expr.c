a60918c8326014e402cda3b7511d679fbb7f7659
tracer-ics2024 <tracer@njuics.org>
2024-09-28 16:34:37 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:34:37 up  1:09,  3 users,  load average: 0.08, 0.07, 0.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -251,6 +251,9 @@ static word_t eval(int p, int q, bool *success) {
       case '-': return val1 - val2;
       case '*': return val1 * val2;
       case '/': Assert(val2 != 0, "Divisor can't be 0");  return val1 / val2;
+      case TK_AND: return val1 && val2;
+      case TK_EQ: return val1 == val2;
+      case TK_UEQ: return val1 != val2;
       default: assert(0);
     }
   }
