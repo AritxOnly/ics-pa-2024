4380fdbb9f465b68e8d8c1093c733f0b0091a46b
tracer-ics2024 <tracer@njuics.org>
2024-10-03 21:22:44 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:22:44 up  1:13,  4 users,  load average: 0.03, 0.04, 0.04

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -28,7 +28,8 @@
       || tokens[i].type == '*' || tokens[i].type == '/' \
       || tokens[i].type == TK_BRACKET_L || tokens[i].type == TK_BRACKET_R \
       || tokens[i].type == TK_AND || tokens[i].type == TK_EQ \
-      || tokens[i].type == TK_UEQ || tokens[i].type == TK_DEREF)
+      || tokens[i].type == TK_UEQ || tokens[i].type == TK_DEREF \
+      || tokens[i].type == TK_NEG)
 
 word_t paddr_read(paddr_t addr, int len);
 
