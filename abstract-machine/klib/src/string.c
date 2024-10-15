971502952e559dc1e56ece497a959d8abff97e8d
tracer-ics2024 <tracer@njuics.org>
2024-10-15 21:21:55 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:21:55 up  3:03,  3 users,  load average: 0.01, 0.05, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,6 +5,7 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
+  assert(s != NULL);
   size_t cnt = 0;
   while (s[cnt] != '\0') {
     cnt++;
@@ -16,39 +17,109 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  assert(dst != NULL && src != NULL);
+  char *ptr_d = dst;
+  const char *ptr_s = src;
+  while (*ptr_s != '\0') {
+    *ptr_d = *ptr_s;
+    ptr_d++;
+    ptr_s++;
+  }
+  *ptr_d = *ptr_s;
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  assert(dst != NULL && src != NULL);
+  for (int i = 0; i < n; i++) {
+    dst[i] = src[i];
+  }
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  assert(dst != NULL && src != NULL);
+  char *ptr_d = dst;
+  while (*ptr_d != '\0') {
+    ptr_d++;
+  }
+  const char *ptr_s = src;
+  while (*ptr_s != '\0') {
+    *ptr_d = *ptr_s;
+    ptr_d++;
+    ptr_s++;
+  }
+  *ptr_d = *ptr_s;
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  assert(s1 != NULL && s2 != NULL);
+  while (*s1 == *s2) {
+    if (*s1 == '\0') {
+      return 0;
+    }
+    s1++;
+    s2++;
+  }
+  return *s1 - *s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  assert(s1 != NULL && s2 != NULL);
+  for (int i = 0; i < n; i++) {
+    if (s1[i] != s2[i]) {
+      return s1[i] - s2[i];
+    } else if (s1[i] == '\0') {
+      return 0;
+    }
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  // memset是将每个字节都设置为c
+  uint8_t *ptr = s;
+  for (int i = 0; i < n; i++) {
+    *(ptr + i) = (uint8_t)c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  // 如果目标区域和源区域有重叠的话，memmove() 
+  // 能够保证源串在被覆盖之前将重叠区域的字节拷贝到目标区域中，复制后源区域的内容会被更改
+  uint8_t *ptr_d = dst;
+  const uint8_t *ptr_s = src;
+  if (ptr_d > ptr_s) {  // src在dst前面，从后往前拷贝避免重叠
+    for (int i = n - 1; i >= 0; i--) {
+      *(ptr_d + i) = *(ptr_s + i);
+    }
+  } else if (ptr_d < ptr_s) {
+    for (int i = 0; i < n; i++) {
+      *(ptr_d + i) = *(ptr_s + i);
+    }
+  }
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  uint8_t *ptr_o = out;
+  const uint8_t *ptr_i = in;
+  for (int i = 0; i < n; i++) {
+    *(ptr_o + i) = *(ptr_i + i);
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const uint8_t *ptr1 = s1, *ptr2 = s2; 
+  for (int i = 0; i < n; i++) {
+    if (*(ptr1 + i) != *(ptr2 + i)) {
+      return *(ptr1 + i) - *(ptr2 + i);
+    }
+  }
+  return 0;
 }
 
 #endif
