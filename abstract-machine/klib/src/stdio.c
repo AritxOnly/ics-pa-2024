0a49486529d41d73dccb24bee23c8e05ae4a0e23
tracer-ics2024 <tracer@njuics.org>
2024-10-15 21:55:13 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:55:13 up  3:36,  4 users,  load average: 0.08, 0.04, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -3,6 +3,47 @@
 #include <klib-macros.h>
 #include <stdarg.h>
 
+void int_parse(char* dest, int v) {
+  char tmp_str[20];
+  size_t i = 0;
+  if (v < 0) {
+    tmp_str[i++] = '-';
+    v = -v;
+  }
+  while (v != 0) {
+    int digit = v % 10;
+    tmp_str[i++] = digit + '0';
+    v /= 10;
+  }
+  while (i--) {
+    *(dest++) = tmp_str[i];
+  }
+  *dest = '\0';
+}
+
+void int_hex_parse(char* dest, int v) {
+  char tmp_str[20];
+  size_t i = 0;
+  if (v < 0) {
+    tmp_str[i++] = '-';
+    v = -v;
+  }
+  while (v != 0) {
+    int rem = v % 16;
+    if (rem >= 0 && rem <= 9) {
+      tmp_str[i++] = rem + '0';
+    } else {
+      tmp_str[i++] = (rem - 10) + 'A';
+    }
+    v /= 16;
+  }
+  while (i--) {
+    *dest = tmp_str[i];
+    dest++;
+  }
+  *dest = '\0';
+}
+
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
@@ -14,7 +55,40 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);  // 初始化args，其中va_start二参是最后一个确定参数
+  *out = '\0'; // 初始化
+  char parsed[20] = {0};
+  while (*fmt) {
+    switch (*fmt) {
+      case '%':
+        fmt++;
+        switch (*fmt) {
+          case 'd': {
+            int val = va_arg(args, int);
+            int_parse(parsed, val);
+            strcat(out, parsed);
+            break;
+          }
+          case 's': {
+            char* str = va_arg(args, char*);
+            strcat(out, str);
+            break;
+          }
+          case 'x': {
+            int val = va_arg(args, int);
+            int_hex_parse(parsed, val);
+            strcat(out, parsed);
+            break;
+          }
+        }
+        break;
+      default:
+        return -1;
+    }
+  }
+  return strlen(out);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
