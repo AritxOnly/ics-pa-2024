970e26870a5b6cc1bfb9a9cb76a7bb0d0ddacd93
tracer-ics2024 <tracer@njuics.org>
2024-10-17 15:47:10 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:47:10 up  7:52,  3 users,  load average: 0.06, 0.04, 0.06

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -52,7 +52,7 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  IFDEF(CONFIG_MTRACE, log_write("Attempting to read %d bytes of memory at %x", len, addr));
+  IFDEF(CONFIG_MTRACE, log_write("Attempting to read %d bytes at %x", len, addr));
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -60,6 +60,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  IFDEF(CONFIG_MTRACE, log_write("Attempting to write %d bytes at %x", len, addr));
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
