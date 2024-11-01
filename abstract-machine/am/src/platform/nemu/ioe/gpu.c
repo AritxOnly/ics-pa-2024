935ed0ba596c47571bec114f01980cf0b7a4a90c
tracer-ics2024 <tracer@njuics.org>
2024-11-01 21:02:33 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:02:33 up 1 day, 13:20,  5 users,  load average: 0.13, 0.04, 0.01

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <nemu.h>
-#include <stdint.h>
+#include <string.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
@@ -16,9 +16,9 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  int w = (inl(VGACTL_ADDR) >> 16) & 0xff;
-  int h = inl(VGACTL_ADDR) & 0xff;
-  int sz = w * h * sizeof(uint32_t);
+  uint16_t w = (inl(VGACTL_ADDR) >> 16) & 0xffff;
+  uint16_t h = inl(VGACTL_ADDR) & 0xffff;
+  uint32_t sz = w * h * sizeof(uint32_t);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = w, .height = h,
@@ -27,6 +27,17 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint16_t w = (inl(VGACTL_ADDR) >> 16) & 0xffff;
+
+  if (!ctl->pixels || ctl->w == 0 || ctl->h == 0) return; 
+
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = ctl->pixels;  // 头指针
+
+  for (int y = 0; y < ctl->h; y++) {
+    memcpy(&fb[w * (ctl->y + y) + ctl->x], &pixels[ctl->w * y], ctl->w * sizeof(uint32_t));
+  }
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1); // 写入sync寄存器
   }
