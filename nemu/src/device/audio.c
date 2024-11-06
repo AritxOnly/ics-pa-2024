83354c1de71740b8c7f29b7f1c5bc6cfde0f40d0
tracer-ics2024 <tracer@njuics.org>
2024-11-06 15:36:18 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:36:18 up 17:18,  4 users,  load average: 0.33, 0.16, 0.05

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -30,7 +30,57 @@ enum {
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
+struct Stream_Buf {
+  uint32_t start;
+  uint32_t end;
+} sbuf_area = { .start = 0 };
+
+static SDL_AudioSpec audio_spec = {}; // 音频信息
+
+void audio_callback(void *userdata, uint8_t *stream, int len) {
+  SDL_memset(stream, 0, len); // 清空流缓冲区，防止存在杂音
+  uint32_t count = audio_base[reg_count]; // 当前流缓冲区的大小
+  if (count == 0) return;
+  int actual_len = (len > count) ? count : len;
+  // 写入流缓冲区
+  SDL_memcpy(stream, sbuf + sbuf_area.start, actual_len);
+  sbuf_area.start = (sbuf_area.start + actual_len) % audio_base[reg_sbuf_size];
+  audio_base[reg_count] -= actual_len;
+}
+
+static void audio_system_init() {
+  audio_spec.format = AUDIO_S16SYS;
+  audio_spec.userdata = NULL;
+  audio_spec.freq = audio_base[reg_freq];
+  audio_spec.channels = audio_base[reg_channels];
+  audio_spec.samples = audio_base[reg_samples];
+  audio_spec.callback = audio_callback;
+
+  // 初始化音频子系统
+  if (SDL_InitSubSystem(SDL_INIT_AUDIO) != 0) {
+    panic("Audio Sub-syetem initialization failed!!!");
+  }
+  // 打开音频设备
+  if (SDL_OpenAudio(&audio_spec, NULL) != 0) {
+    panic("SDLOpenAudio failed!!!");
+  }
+  SDL_PauseAudio(0);
+  audio_base[reg_init] = 0;
+}
+
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  uint32_t reg_idx = offset / 4;
+  if (is_write) {
+    switch (reg_idx) {
+      case reg_init:
+      case reg_freq:
+      case reg_samples:
+      case reg_channels:
+        audio_system_init();
+        break;
+      default: break;
+    }
+  }
 }
 
 void init_audio() {
@@ -44,4 +94,7 @@ void init_audio() {
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+  sbuf_area.end = audio_base[reg_sbuf_size];
 }
