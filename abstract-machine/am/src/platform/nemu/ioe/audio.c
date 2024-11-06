5d0af90cdd40cf09b5a0fbbc9f2f25d0edeb8d43
231098306-Zhou Qing <aritx_zhou@qq.com>
2024-11-06 14:08:50 +0800

    am & nemu audio: reset to default and rebuild

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,6 +1,5 @@
 #include <am.h>
 #include <nemu.h>
-#include <klib.h>
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
@@ -9,68 +8,19 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
-#define SBUF_ADDR AUDIO_SBUF_ADDR
-#define SBUF_SIZE 0x10000
-
-static uint32_t sbuf_size = 0;
-static uint32_t write_pos = 0;
-
-void memcpy_to_mmio(uintptr_t dest, void *src, size_t n) {
-  uint8_t *data = (uint8_t *)src;
-  for (size_t i = 0; i < n; i++) {
-    outb(dest + i, data[i]);
-  }
-}
-
 void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = true;
-  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+  cfg->present = false;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
-  outl(AUDIO_FREQ_ADDR, ctrl->freq);
-  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
-  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
-  outl(AUDIO_INIT_ADDR, 1); // 初始化
-  sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = inl(AUDIO_COUNT_ADDR);
+  stat->count = 0;
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-  uint32_t len = ctl->buf.end - ctl->buf.start;
-  uint8_t *data = (uint8_t *)ctl->buf.start;
-
-  while (len > 0) {
-    uint32_t free_space = sbuf_size - inl(AUDIO_COUNT_ADDR);
-    if (free_space == 0) {
-      // 缓冲区已满，等待
-      continue;
-    }
-
-    uint32_t write_len = (len < free_space) ? len : free_space;
-    uint32_t first_chunk = (write_pos + write_len <= sbuf_size) ? write_len : sbuf_size - write_pos;
-
-    memcpy_to_mmio(SBUF_ADDR + write_pos, data, first_chunk);
-    if (write_len > first_chunk) {
-      memcpy_to_mmio(SBUF_ADDR, data + first_chunk, write_len - first_chunk);
-      write_pos = write_len - first_chunk;
-    } else {
-      write_pos += first_chunk;
-    }
-
-    write_pos %= sbuf_size;
-
-    data += write_len;
-    len -= write_len;
-
-    // 更新 count 寄存器
-    uint32_t current_count = inl(AUDIO_COUNT_ADDR);
-    outl(AUDIO_COUNT_ADDR, current_count + write_len);
-  }
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -1,5 +1,5 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
 *
 * NEMU is licensed under Mulan PSL v2.
 * You can use this software according to the terms and conditions of the Mulan PSL v2.
@@ -16,7 +16,6 @@
 #include <common.h>
 #include <device/map.h>
 #include <SDL2/SDL.h>
-#include <pthread.h>
 
 enum {
   reg_freq,
@@ -31,101 +30,10 @@ enum {
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
-static SDL_AudioSpec sdl_spec = {};   // SDL 音频规格
-static uint32_t sbuf_size = CONFIG_SB_SIZE;  // 缓冲区大小
-static uint32_t count = 0;            // 缓冲区中数据的字节数
-static uint32_t read_pos = 0;         // 读取位置
-
-static pthread_mutex_t audio_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-static void audio_callback(void *userdata, Uint8 *stream, int len) {
-  int remain = len;
-
-  while (remain > 0) {
-    pthread_mutex_lock(&audio_mutex);
-    int available = (count < remain) ? count : remain;
-    pthread_mutex_unlock(&audio_mutex);
-
-    if (available > 0) {
-      if (read_pos + available <= sbuf_size) {
-        memcpy(stream + (len - remain), sbuf + read_pos, available);
-        read_pos += available;
-      } else {
-        int first_chunk = sbuf_size - read_pos;
-        memcpy(stream + (len - remain), sbuf + read_pos, first_chunk);
-        memcpy(stream + (len - remain) + first_chunk, sbuf, available - first_chunk);
-        read_pos = available - first_chunk;
-      }
-
-      read_pos %= sbuf_size;
-
-      pthread_mutex_lock(&audio_mutex);
-      count -= available;
-      pthread_mutex_unlock(&audio_mutex);
-    } else {
-      memset(stream + (len - remain), 0, remain);
-      remain = 0;
-      break;
-    }
-
-    remain -= available;
-  }
-}
-
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
-  if (is_write) {
-    uint32_t reg_index = offset / 4;
-    uint32_t data = *(uint32_t *)((uint8_t *)audio_base + offset);
-
-    switch (reg_index) {
-      case reg_freq:
-        sdl_spec.freq = data;
-        break;
-      case reg_channels:
-        sdl_spec.channels = data;
-        break;
-      case reg_samples:
-        sdl_spec.samples = data;
-        break;
-      case reg_init:
-        if (data) {
-          sdl_spec.format = AUDIO_S16SYS;  // 16 位有符号系统字节序
-          sdl_spec.callback = audio_callback;
-          sdl_spec.userdata = NULL;
-
-          if (SDL_OpenAudio(&sdl_spec, NULL) < 0) {
-            panic("SDL_OpenAudio: %s", SDL_GetError());
-          }
-
-          SDL_PauseAudio(0);  // 开始播放音频
-          audio_base[reg_sbuf_size] = sbuf_size;
-        }
-        break;
-      default:
-        break;
-    }
-  } else {
-    uint32_t reg_index = offset / 4;
-    switch (reg_index) {
-      case reg_count:
-        pthread_mutex_lock(&audio_mutex);
-        audio_base[reg_count] = count;
-        pthread_mutex_unlock(&audio_mutex);
-        break;
-      case reg_sbuf_size:
-        audio_base[reg_sbuf_size] = sbuf_size;
-        break;
-      default:
-        break;
-    }
-  }
 }
 
 void init_audio() {
-  if (SDL_Init(SDL_INIT_AUDIO) != 0) {
-    panic("Failed to initialize SDL: %s", SDL_GetError());
-  }
-
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
 #ifdef CONFIG_HAS_PORT_IO
