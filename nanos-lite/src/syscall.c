5742fccd54a44eb73fc16b409cb2b3e97235840e
tracer-ics2024 <tracer@njuics.org>
2024-11-19 10:01:24 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:01:24 up  1:41,  3 users,  load average: 0.01, 0.02, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -34,7 +34,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_yield: yield(); c->GPRx = 0; break;
     case SYS_exit: halt(a[1]); break;
-    case SYS_write: c->GPRx = fs_write(a[0], (void *)a[1], a[2]); break;
+    case SYS_write: c->GPRx = fs_write(a[1], (void *)a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
