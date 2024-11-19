06a4170f4bd01468e15a01618599dcc6c68ddd00
tracer-ics2024 <tracer@njuics.org>
2024-11-19 09:56:15 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:56:15 up  1:36,  3 users,  load average: 0.04, 0.05, 0.01

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -23,14 +23,37 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
+size_t stdout_write(const void *buf, size_t offset, size_t len) {
+  const char *ch_buf = buf;
+  for (size_t i = 0; i < len; i++) {
+    putch(ch_buf[i]);
+  }
+  return len;
+}
+
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, stdout_write},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, stdout_write},
 #include "files.h"
 };
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+#define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
+
+void fs_write(int fd, const void *buf, size_t len) {
+  if (fd < 0 || fd >= NR_FILES) {
+    panic("Invalid 'fd' value %d", fd);
+  }
+  
+  Finfo *f = &file_table[fd];
+  if (f->write) {
+    f->write(buf, 0, len);
+  } else {
+    panic("Unimplemented write");
+  }
 }
