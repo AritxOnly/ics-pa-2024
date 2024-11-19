080d3480aa783f7dd341d155020a12dedaeb68a1
tracer-ics2024 <tracer@njuics.org>
2024-11-19 13:00:34 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:00:34 up  4:40,  4 users,  load average: 0.03, 0.03, 0.00

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -12,8 +12,15 @@ typedef struct {
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
-  size_t inner_offset;
 } Finfo;
+
+typedef int off_t;
+
+typedef struct {
+  Finfo *file;
+  off_t offset;
+  bool opened;
+} OpenFile;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
@@ -43,6 +50,9 @@ static Finfo file_table[] __attribute__((used)) = {
 #include "files.h"
 };
 
+#define MAX_OPEN_FILES 128
+static OpenFile open_files[MAX_OPEN_FILES];
+
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
@@ -61,56 +71,106 @@ int fs_open(const char *pathname, int flags, int mode) {
   if (fd == -1) {
     panic("File %s not found", pathname);
   }
+  open_files[fd].file = &file_table[fd];
+  open_files[fd].offset = 0;
+  open_files[fd].opened = true;
   return fd;
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
-  if (fd < 0 || fd >= NR_FILES) {
+  if (fd < 0 || fd >= NR_FILES || !open_files[fd].opened) {
     panic("Invalid 'fd' value %d", fd);
   }
 
-  Finfo *f = &file_table[fd];
-  size_t read_len = -1;
-  size_t offset = f->disk_offset + f->inner_offset;
-  len = (f->size - f->inner_offset > len) ? len : (f->size - f->inner_offset);
+  OpenFile *of = &open_files[fd];
+  Finfo *f = of->file;
+  size_t read_len = 0;
+  size_t offset = f->disk_offset + of->offset;
+
+  if (of->offset >= f->size) {
+    return 0;
+  }
+
+  // 计算实际可读取的长度，防止越界
+  size_t max_len = f->size - of->offset;
+  if (len > max_len) {
+    len = max_len;
+  }
+
+  if (len == 0) {
+    return 0;
+  }
 
   if (f->read) {
     read_len = f->read(buf, offset, len);
   } else {
     read_len = ramdisk_read(buf, offset, len);
   }
+  of->offset += read_len;
   return read_len;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
-  if (fd < 0 || fd >= NR_FILES) {
+  if (fd < 0 || fd >= NR_FILES || !open_files[fd].opened) {
     panic("Invalid 'fd' value %d", fd);
   }
-  
-  Finfo *f = &file_table[fd];
-  if (f->write) {
-    size_t write_len = f->write(buf, f->inner_offset, len);
-    f->inner_offset += write_len;
-    return write_len;
+
+  OpenFile *of = &open_files[fd];
+  Finfo *f = of->file;
+  size_t write_len = 0;
+  size_t offset = f->disk_offset + of->offset;
+
+  if (of->offset >= f->size) {
+    return 0;
   }
-  return -1;
+
+  size_t max_len = f->size - of->offset;
+  if (len > max_len) {
+    len = max_len;
+  }
+
+  if (len == 0) {
+    return 0;
+  }
+
+  if (f->read) {
+    write_len = f->write(buf, offset, len);
+  } else {
+    write_len = ramdisk_write(buf, offset, len);
+  }
+  of->offset += write_len;
+  return write_len;
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
-  if (fd < 0 || fd >= NR_FILES) {
+  if (fd < 0 || fd >= NR_FILES || !open_files[fd].opened) {
     panic("Invalid 'fd' value %d", fd);
   }
 
-  Finfo *f = &file_table[fd];
+  OpenFile *of = &open_files[fd];
+  Finfo *f = of->file;
+  off_t new_offset;
+
   switch (whence) {
-    case SEEK_SET: f->inner_offset = offset; break;
-    case SEEK_CUR: f->inner_offset += offset; break;
-    case SEEK_END: 
-      f->inner_offset = (offset > 0) ? f->inner_offset : f->size + offset; 
+    case SEEK_SET:
+      new_offset = offset;
       break;
-    default: panic("Invalid whence %d", whence);
+    case SEEK_CUR:
+      new_offset = of->offset + offset;
+      break;
+    case SEEK_END:
+      new_offset = f->size + offset;
+      break;
+    default:
+      panic("Invalid whence %d", whence);
   }
-  return f->inner_offset;
+
+  if (new_offset < 0 || new_offset > f->size) {
+    panic("Invalid offset after lseek");
+  }
+
+  of->offset = new_offset;
+  return of->offset;
 }
 
 int fs_close(int fd) {
