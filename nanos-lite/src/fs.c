4481597841a259898c5a705104e3299e954203ff
tracer-ics2024 <tracer@njuics.org>
2024-11-20 17:10:12 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:10:12 up 1 day,  1:02,  3 users,  load average: 0.01, 0.03, 0.05

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -153,7 +153,7 @@ size_t fs_lseek(int fd, size_t offset, int whence) {
 
   OpenFile *of = &open_files[fd];
   Finfo *f = of->file;
-  Log("Opened file %s, size = %u", f->name, f->size);
+  Log("Opened file %s, size = %d", f->name, f->size);
   off_t new_offset;
 
   switch (whence) {
