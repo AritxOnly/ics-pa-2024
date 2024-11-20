23752b889233d09d1f1d0db992af4eee8e2068da
tracer-ics2024 <tracer@njuics.org>
2024-11-20 23:54:01 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:54:01 up  3:08,  5 users,  load average: 0.03, 0.07, 0.08

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -7,19 +7,18 @@ int main() {
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
+  printf("ftell %d", (int)size);
   assert(size == 5000);
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
-    printf("Running fscanf %d\n", i);
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
-    printf("Running fprintf %d\n", i);
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
