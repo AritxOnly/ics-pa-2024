e631ff1009ed0224a284b1b6399e61f5b4aa0d03
tracer-ics2024 <tracer@njuics.org>
2024-11-21 21:58:17 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:58:17 up 1 day,  1:13,  4 users,  load average: 0.22, 0.08, 0.02

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -16,7 +16,7 @@ typedef struct {
   bool opened;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENTS, FD_FB, FD_FBINFO};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENTS, FD_FBINFO, FD_FB};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
