5f2a62e66971ad339de2e4527885541e85f09337
tracer-ics2024 <tracer@njuics.org>
2024-11-21 15:11:51 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:11:51 up 18:26,  4 users,  load average: 0.03, 0.05, 0.00

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,6 +25,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T kv = io_read(AM_INPUT_KEYBRD);
   if (kv.keycode != AM_KEY_NONE) {
+    Log("write %08x, %s", (intptr_t)buf, keyname[kv.keycode]);
     return snprintf(buf, len, "k%c %s", (kv.keydown ? 'd' : 'u'), keyname[kv.keydown]);
   }
   return 0;
