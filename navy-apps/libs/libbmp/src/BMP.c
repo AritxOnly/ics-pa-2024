b8d0639925bdbc785ac9f5ac9465fc72207fc370
tracer-ics2024 <tracer@njuics.org>
2024-11-21 21:51:20 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:51:20 up 1 day,  1:06,  4 users,  load average: 0.12, 0.05, 0.01

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -34,8 +34,6 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
   int depth = (hdr.bitcount == 32 ? 4 : 3);
 
-  printf("%p\n", pixels);
-
   int line_off = (depth == 4 ? w * 4 : (w * 3 + 3) & ~0x3);
   for (int i = 0; i < h; i ++) {
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
