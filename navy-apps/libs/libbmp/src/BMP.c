16fd1f955ca80ee30fe7727ce4aa2f97bd3c4719
tracer-ics2024 <tracer@njuics.org>
2024-11-21 21:03:03 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:03:03 up 1 day, 18 min,  4 users,  load average: 0.00, 0.00, 0.00

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -42,7 +42,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
       uint8_t b = *(((uint8_t*)&pixels[w * i]) + depth * j);
       uint8_t g = *(((uint8_t*)&pixels[w * i]) + depth * j + 1);
       uint8_t r = *(((uint8_t*)&pixels[w * i]) + depth * j + 2);
-      printf("reading i %d, j %d, w %d, h %d, w * i %d", i, j, w, h, w * i);
+      // printf("reading i %d, j %d, w %d, h %d, w * i %d", i, j, w, h, w * i);
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
     }
   }
