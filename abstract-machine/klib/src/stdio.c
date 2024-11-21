349ef2b353602087506057bc39db1dcd93dcbc25
tracer-ics2024 <tracer@njuics.org>
2024-11-21 18:52:17 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:52:17 up 22:07,  4 users,  load average: 0.02, 0.03, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -67,101 +67,15 @@ static inline int isdigit(unsigned char ch) {
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int vsprintf(char *out, const char *fmt, va_list args) {
-  char *str = out;
-  const char *p = fmt;
-  char ch;
-  while ((ch = *p++)) {
-    if (ch != '%') {
-      *str++ = ch;
-      continue;
-    }
-    if (ch == '\0') {
-      break;
-    }
-    ch = *p++;
-    char space = ' ';
-    int cnt = 0;
-    if (ch == '0') {
-      space = ch;
-      ch = *p++;
-    }
-    if (isdigit((unsigned char)ch)) {
-      cnt = 0;
-      while (isdigit((unsigned char)ch)) {
-        // putch('l'); putch('\n');
-        cnt = cnt * 10 + (ch - '0');
-        ch = *p++;
-      }
-    }
-    switch (ch) {
-      case 'd': {
-        int val = va_arg(args, int);
-        char buf[PRESAVE_BUFFER];
-        int_parse(buf, val);
-        if (val < 0 && space == '0') {
-          pre_space(buf + 1, space, cnt - 1, PRESAVE_BUFFER - 1);
-        } else {
-          pre_space(buf, space, cnt, PRESAVE_BUFFER);
-        }
-        strcpy(str, buf);
-        str += strlen(buf);
-        break;
-      }
-      case 's': {
-        char *val = va_arg(args, char *);
-        strcpy(str, val);
-        str += strlen(val);
-        break;
-      }
-      case 'x': {
-        uintptr_t val = va_arg(args, uintptr_t);
-        char buf[PRESAVE_BUFFER];
-        int_hex_parse(buf, val);
-        pre_space(buf, space, cnt, PRESAVE_BUFFER);
-        strcpy(str, buf);
-        str += strlen(buf);
-        break;
-      }
-      case 'p': {
-        void *ptr = va_arg(args, void *);
-        uintptr_t val = (uintptr_t)ptr;
-        char buf[PRESAVE_BUFFER];
-        strcpy(buf, "0x");
-        int_hex_parse(buf + 2, val);
-        pre_space(buf, space, cnt, PRESAVE_BUFFER);
-        strcpy(str, buf);
-        str += strlen(buf);
-        break;
-      }
-      default:
-        *str++ = '%';
-        *str++ = ch;
-        break;
-    }
-  }
-  *str = '\0';
-  return str - out;
+  // 使用一个较大的值，表示无限制长度
+  return vsnprintf(out, SIZE_MAX, fmt, args);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
-  int len = vsprintf(out, fmt, args);
+  int len = vsnprintf(out, SIZE_MAX, fmt, args);
   va_end(args);
-  return len;
-}
-
-#define BUFFER_MAX 8192
-
-int printf(const char *fmt, ...) {
-  char buffer[BUFFER_MAX];
-  va_list args;
-  va_start(args, fmt);
-  int len = vsprintf(buffer, fmt, args);
-  va_end(args);
-  for (int i = 0; i < len; i++) {
-    putch(buffer[i]);
-  }
   return len;
 }
 
@@ -173,13 +87,144 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
   return len;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list args) {
-  int len = vsprintf(out, fmt, args);
-  if (len >= n) {
-    out[n - 1] = '\0';
-    len = n - 1;
+#define BUFFER_MAX 8192
+
+int printf(const char *fmt, ...) {
+  char buffer[BUFFER_MAX];
+  va_list args;
+  va_start(args, fmt);
+  int len = vsnprintf(buffer, BUFFER_MAX, fmt, args);
+  va_end(args);
+  for (int i = 0; i < len; i++) {
+    putch(buffer[i]);
   }
   return len;
+}
+
+int vsnprintf(char *out, size_t n, const char *fmt, va_list args) {
+  char *str = out;
+  const char *p = fmt;
+  size_t remaining = n;
+  char ch;
+
+  while ((ch = *p++) != '\0') {
+    if (ch != '%') {
+      if (remaining > 1) {
+        *str++ = ch;
+        remaining--;
+      } else if (remaining == 1) {
+        // 缓冲区只剩下空间放 '\0'，不再写入字符
+        remaining--;
+      }
+      continue;
+    }
+
+    ch = *p++;
+    if (ch == '\0') {
+      // 格式字符串以 '%' 结束，输出 '%'
+      if (remaining > 1) {
+        *str++ = '%';
+        remaining--;
+      }
+      break;
+    }
+
+    char space = ' ';
+    int cnt = 0;
+
+    // 处理可能的填充字符和宽度
+    if (ch == '0') {
+      space = '0';
+      ch = *p++;
+    }
+
+    if (isdigit((unsigned char)ch)) {
+      cnt = 0;
+      while (isdigit((unsigned char)ch)) {
+        cnt = cnt * 10 + (ch - '0');
+        ch = *p++;
+      }
+    }
+
+    switch (ch) {
+      case 'd': {
+        int val = va_arg(args, int);
+        char buf[PRESAVE_BUFFER];
+        int_parse(buf, val);
+
+        if (val < 0 && space == '0') {
+          pre_space(buf + 1, space, cnt - 1, PRESAVE_BUFFER - 1);
+        } else {
+          pre_space(buf, space, cnt, PRESAVE_BUFFER);
+        }
+
+        size_t len = strlen(buf);
+        size_t copy_len = (len < remaining - 1) ? len : remaining - 1;
+        strncpy(str, buf, copy_len);
+        str += copy_len;
+        remaining -= copy_len;
+        break;
+      }
+
+      case 's': {
+        char *val = va_arg(args, char *);
+        if (val == NULL) val = "(null)"; // 防止 val 为 NULL
+        size_t len = strlen(val);
+        size_t copy_len = (len < remaining - 1) ? len : remaining - 1;
+        strncpy(str, val, copy_len);
+        str += copy_len;
+        remaining -= copy_len;
+        break;
+      }
+
+      case 'x': {
+        uintptr_t val = va_arg(args, uintptr_t);
+        char buf[PRESAVE_BUFFER];
+        int_hex_parse(buf, val);
+        pre_space(buf, space, cnt, PRESAVE_BUFFER);
+
+        size_t len = strlen(buf);
+        size_t copy_len = (len < remaining - 1) ? len : remaining - 1;
+        strncpy(str, buf, copy_len);
+        str += copy_len;
+        remaining -= copy_len;
+        break;
+      }
+
+      case 'p': {
+        void *ptr = va_arg(args, void *);
+        uintptr_t val = (uintptr_t)ptr;
+        char buf[PRESAVE_BUFFER];
+        strcpy(buf, "0x");
+        int_hex_parse(buf + 2, val);
+        pre_space(buf, space, cnt, PRESAVE_BUFFER);
+
+        size_t len = strlen(buf);
+        size_t copy_len = (len < remaining - 1) ? len : remaining - 1;
+        strncpy(str, buf, copy_len);
+        str += copy_len;
+        remaining -= copy_len;
+        break;
+      }
+
+      default:
+        if (remaining > 1) {
+          *str++ = '%';
+          remaining--;
+        }
+        if (remaining > 1) {
+          *str++ = ch;
+          remaining--;
+        }
+        break;
+    }
+  }
+
+  if (n > 0) {
+    *str = '\0';
+  }
+
+  return str - out;
 }
 
 #endif
