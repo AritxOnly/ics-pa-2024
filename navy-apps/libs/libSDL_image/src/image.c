cc8e9ab31ea277539fe430e454e4550043dd57b8
tracer-ics2024 <tracer@njuics.org>
2024-12-03 11:05:21 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:05:21 up 18:58,  3 users,  load average: 1.83, 1.57, 1.03

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -15,11 +15,29 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 
 SDL_Surface* IMG_Load(const char *filename) {
   FILE* fp = fopen(filename, "r");
-  long filesz = fseek(fp, 0, SEEK_SET);
-  if (filesz <= 0)  return NULL;
+  if (!fp) {
+    fprintf(stderr, "Failed to open %s\n", filename);
+    return NULL;
+  }
+  if (fseek(fp, 0, SEEK_END) != 0) {
+    fprintf(stderr, "Failed to read %s\n", filename);
+    fclose(fp);
+    return NULL;
+  }
+  long filesz = ftell(fp);
+  if (filesz <= 0)  {
+    fprintf(stderr, "Failed to get filesz = %d\n", (int)filesz);
+    return NULL;
+  }
+
   uint8_t *buf = malloc(filesz);
-  fread(buf, 1, filesz, fp);
-  printf("reached here\n");
+  if (!buf) {
+    fprintf(stderr, "Failed to allocate buffer\n");
+    return NULL;
+  }
+  fseek(fp, 0, SEEK_SET);
+  int rd = fread(buf, 1, filesz, fp);
+  assert(rd == filesz);
   SDL_Surface *s = STBIMG_LoadFromMemory(buf, filesz);
   fclose(fp);
   return s;
