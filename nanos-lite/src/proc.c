7b19d2ade03c6a12b6885989922568fa11a42922
tracer-ics2024 <tracer@njuics.org>
2025-01-14 13:15:18 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:15:18 up 22:57,  5 users,  load average: 1.08, 1.15, 1.08

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -37,14 +37,28 @@ Context *context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
 }
 
 void init_proc() {
+  context_kload(&pcb[0], hello_fun, NULL);
+  context_kload(&pcb[1], hello_fun, NULL);
   switch_boot_pcb();
 
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, ENTRY_BIN);
+  // naive_uload(NULL, ENTRY_BIN);
 }
 
+static int current_proc = 0;
 Context* schedule(Context *prev) {
-  return NULL;
+  if (prev) {
+    pcb[current_proc].cp = prev;  // 保存现场到该PCB
+  }
+
+  // 切换到下一个非空的PCB进程
+  do {
+    current_proc = (current_proc + 1) % MAX_NR_PROC;
+    current = &pcb[current_proc];   // 更新全局指针
+  } while (!current);
+
+  // 返回下一个进程的cp，进入新的上下文
+  return current->cp;
 }
