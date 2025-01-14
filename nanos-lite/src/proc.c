12d593315966d077daf40b03731598422a41fc6e
tracer-ics2024 <tracer@njuics.org>
2025-01-14 16:36:26 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:36:26 up 1 day,  2:18,  5 users,  load average: 0.51, 0.66, 0.89

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -5,6 +5,7 @@
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
+static int curr_idx=3;
 
 void switch_boot_pcb() {
   current = &pcb_boot;
@@ -19,46 +20,33 @@ void hello_fun(void *arg) {
   }
 }
 
-void naive_uload(PCB *, const char *);
-
-void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
-  // 通过 pcb->stack 来提供栈区域
-  Area kstack = {
-    .start = pcb->stack,
-    .end   = pcb->stack + STACK_SIZE,
-  };
-
-  // 调用 kcontext() 在这片栈区里创建上下文
-  Context *context = kcontext(kstack, entry, arg);
-
-  // 记录到 pcb->cp 里
-  pcb->cp = context;
-}
+void context_kload(PCB* n_pcb, void (*entry)(void *), void *arg);
 
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, NULL);
-  context_kload(&pcb[1], hello_fun, NULL);
+  context_kload(&pcb[0], hello_fun, (void*)0);
+  context_kload(&pcb[1], hello_fun, (void*)1);
   switch_boot_pcb();
 
   Log("Initializing processes...");
 
+  //naive_uload(NULL, IMAGE_FILE);
+
   // load program here
-  naive_uload(NULL, ENTRY_BIN);
+
 }
 
-static int current_proc = 0;
+void context_kload(PCB* n_pcb, void (*entry)(void *), void *arg) {
+  n_pcb->cp = kcontext((Area) { n_pcb->stack, n_pcb + 1 }, entry, arg);
+}
 
 Context* schedule(Context *prev) {
-  if (prev) {
-    pcb[current_proc].cp = prev;  // 保存现场到该PCB
+  current->cp = prev;
+  while(1) {
+    curr_idx = (curr_idx+1)%4;
+    if (pcb[curr_idx].cp != 0) {
+      current = &pcb[curr_idx];
+      break;
+    }
   }
-
-  // 切换到下一个非空的PCB进程
-  do {
-    current_proc = (current_proc + 1) % MAX_NR_PROC;
-    current = &pcb[current_proc];   // 更新全局指针
-  } while (!current->cp);
-
-  // 返回下一个进程的cp，进入新的上下文
   return current->cp;
 }
