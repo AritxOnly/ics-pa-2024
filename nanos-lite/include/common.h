a05e92f42869ad4d1acf97f988802f795383bfe0
tracer-ics2024 <tracer@njuics.org>
2025-01-14 16:47:15 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:47:15 up 1 day,  2:29,  5 users,  load average: 1.01, 0.74, 0.79

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -34,4 +34,6 @@ size_t events_read(void *buf, size_t offset, size_t len);
 size_t fb_write(const void *buf, size_t offset, size_t len);
 size_t dispinfo_read(void *buf, size_t offset, size_t len);
 
+Context* schedule(Context *prev);
+
 #endif
