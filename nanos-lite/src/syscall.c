ac8e9d08b2f106c0ec8062a0afd9769ca42a2718
tracer-ics2024 <tracer@njuics.org>
2025-01-22 19:19:07 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:19:07 up 11:49,  3 users,  load average: 1.24, 1.12, 0.89

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -42,13 +42,13 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_yield: yield(); c->GPRx = 0; break;
     case SYS_exit: 
-      // if(strcmp("/bin/nterm", ENTRY_BIN) == 0 && strcmp("/bin/nterm", cur_bin) != 0) {
-      //   strncpy(cur_bin, "/bin/nterm", 11);
-      //   //naive_uload(NULL, "/bin/nterm");
-      //   context_uload(current, "/bin/nterm", (char *const[]){NULL}, (char *const[]){NULL});
-      //   switch_boot_pcb();
-      //   yield();
-      // }
+      if(strcmp("/bin/nterm", ENTRY_BIN) == 0 && strcmp("/bin/nterm", cur_bin) != 0) {
+        strncpy(cur_bin, "/bin/nterm", 11);
+        //naive_uload(NULL, "/bin/nterm");
+        context_uload(current, "/bin/nterm", (char *const[]){NULL}, (char *const[]){NULL});
+        switch_boot_pcb();
+        yield();
+      }
 
       // halt(a[1]);  
       c->GPRx = 0;
