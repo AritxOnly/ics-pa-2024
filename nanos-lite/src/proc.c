6196357b7c03bcb965723348aa61c75b0e7a4e3a
tracer-ics2024 <tracer@njuics.org>
2025-01-22 18:30:36 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:30:36 up 11:01,  2 users,  load average: 0.44, 0.19, 0.07

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -42,8 +42,10 @@ Context* schedule(Context *prev) {
   do {
     current_proc = (current_proc + 1) % MAX_NR_PROC;
     current = &pcb[current_proc];   // 更新全局指针
-    // Log("current_proc = %d, current = %p, current->cp = %p", current_proc, current, current->cp);
+    Log("current_proc = %d, current = %p, current->cp = %p", current_proc, current, current->cp);
   } while (!current->cp);
+
+  Log("mepc: %p", current->cp->mepc);
 
   // 返回下一个进程的cp，进入新的上下文
   return current->cp;
