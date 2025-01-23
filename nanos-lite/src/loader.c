ca8d899df894d1fec2ab3e93649bf2d24a1a5b4d
tracer-ics2024 <tracer@njuics.org>
2025-01-23 22:01:51 +0800

    >  compile NEMU 231098306 周擎 Linux aritxonly-ThinkBook-16p 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:01:51 up 11 min,  1 user,  load average: 0.42, 0.61, 0.38

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -133,7 +133,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 
   Context *context = ucontext(NULL, kstack, (void *)entry);
 
-  uintptr_t sp = (uintptr_t)new_page(8);
+  uintptr_t sp = (uintptr_t)(new_page(8) + 8 * PGSIZE - 1);
   sp -= UNSPECIFIED_MEMORY;
 
   Log("Current user stack: sp = %p", sp);
